0477c2e444021c80498a80e709726b49d140a921
CanNotToCode <vestacaroli@gmail.com>
2015-12-09 22:10:43 +0300

    Create O1.pas

---
+++ O1.pas
@@ -0,0 +1,50 @@
+type
+  Mass = array of real;
+ var 
+  Numbers:Mass;
+  n:integer;
+  
+ procedure Reading(var Numbers:Mass; var n:integer);
+  var 
+   i:integer;
+  begin
+  writeln('Input length of array');
+  read(n);
+  SetLength(Numbers,n);
+  writeln('Input array');
+  for i:=0 to n-1 do
+   read(Numbers[i]);
+  end;
+  
+  procedure Bubble( var Numbers:mass; n:integer);
+   var i,j:integer;
+    temp:real;
+   begin
+   for i:=n-1 downto 0 do
+    begin
+     for j:=0 to n-2 do
+     if Numbers[j]>Numbers[j+1] then
+      begin
+      temp:=Numbers[j];
+      Numbers[j]:=Numbers[j+1];
+      Numbers[j+1]:=temp;
+      end;
+     end;
+    end;
+    
+    procedure Output(Numbers:Mass; n:integer);
+     var i:integer;
+     begin
+      write('Sorted array: ');
+      for i:= 0 to n-1 do
+       write(Numbers[i], ' ');
+      end;
+     
+     BEGIN
+      Reading(Numbers,n);
+      Bubble(Numbers,n);
+      Output(Numbers,n);
+     END.
+     
+  
+ 
