46354890f98da8ca09096316203679ee03727c94
CanNotToCode <vestacaroli@gmail.com>
2015-12-11 11:32:51 +0300

    Update OF1.pas

--- OF1.pas
+++ OF1.pas
@@ -9,18 +9,23 @@ var
   num: real;
 begin
   Assign(F1, 'F1.dat');
-  Assign(F2, 'F2.dat'); 
-  readln(n);
+  Assign(F2, 'F2.dat');
   
+  writeln('Input length of the first array');
+  readln(n);
   ReWrite(F1);
+  writeln('Input the first array');
   for i := 0 to n - 1 do
   begin
     read(num);
     write(F1, num);
   end;
   close(f1);
+  
+  writeln('Input length of the second array');
   readln(m);
   Rewrite(f2);
+  writeln('Input the second array');
   for j := 0 to m - 1 do
   begin
     read(num);
@@ -29,40 +34,59 @@ begin
   close(F2);
 end;
 
-function IsIncluded(F1,F2:file of real): boolean;
+
+
+function IsIncluded(F1, F2: file of real): boolean;
 var
-  k, n,  i, j: integer;
-  n1,n2:real;
+  
+  k, n, m, i, j: integer;
+  n1, n2: real;
+  b1,b2:boolean;
+  
 begin
 reset(f1);
 reset(F2);
+  n := Filesize(F1);
   m := FileSize(F2);
+  b1:=true;
+  b2:=true;
   
-  while (not EoF(F1)) and (not EoF(F2))  do
+  while (i <= n - 1) and (j <= m - 1)  do
+  
   begin
-    read(F1,n1);
-    read(F2,n2);
+   if b1 then
+    read(F1, n1);
+   if b2 then
+    read(F2, n2);
+     b1:=false;
+     b2:=false;
     if (n1 <= n2) then 
     begin
       if (n1 = n2) then
       begin
         inc(k);
         inc(j);
-        Seek(F2,j)
+        Seek(F2, j);
+        b2:=true;
       end;
     end
     else
     begin
       inc(j); 
-      Seek(F2,j);
+      Seek(F2, j);
+      b2:=true;
       dec(i);
-      Seek(F1,i);
+      Seek(F1, i);
+      b1:=true;
     end;
     inc(i);
-    Seek(F1,i);
+    Seek(F1, i);
+    b1:=true;
   end;
   IsIncluded := (k = m);
 end;
+
+
 
 begin
   Reading(F1, F2, n, m);
