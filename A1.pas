3edad4fc447ee1a3c1c6279cf5763447d327f3b7
CanNotToCode <vestacaroli@gmail.com>
2015-12-16 18:34:16 +0300

    Update A1.pas

--- A1.pas
+++ A1.pas
@@ -1,87 +1,91 @@
-type 
- Mass = array of integer;
- var
- A1, B1, Rez: array of integer; 
- A, B, P, R, J: string; 
- n, m, pr, s, f, h, v, x, g, d: integer;
-procedure Reading(Var A,B:string; var n,m:integer); 
+//Запись суммы по записям чисел, сложение столбиком
+type
+  Mass = array of integer;
+var
+   A1, B1, Rez: array of integer; 
+   A, B, Rezlt: string; 
+   n, m: integer;
+
+procedure Reading(var A, B: string; var n, m: integer);//считывает записи цифр
+var
+  tempstring: string;
  begin
- writeln('Enter the numbers ');
+  writeln('Enter the numbers ');
   readln(A);
-  read(B);
+  readln(B);
   n := Length(A);
   m := Length(B);
   
-  if (n >= m) then 
+  if (n <= m) then //изменяет строки так, чтобы число с большим числом разрядов хранилось в 1 строке
    begin
-    P := A;
-    R := B;
-    f := n;
-    h := m;
-   end
-  else
-   begin
-    P := B;
-    R := A; 
-    f := m;
-    h := n;
+    tempstring := A;
+    A := B;
+    B := tempstring;
+    n := length(A);
+    m := length(B);
    end;
- end;   
+
+ end;
  
- procedure Conversion (var A1,B1:array of integer; var f,pr,x,g,d,h:integer; var P, R:string); 
-  begin
-   SetLength(A1,f);
-   SetLength(B1,f);
-  for f:=1 to f do
+procedure Conversion(var A1, B1: array of integer; n, m: integer; A, B: string);//процедура преобразует записи чисел в массив их цифр
+ var
+  i, pr, k, l: integer;
+ begin
+   SetLength(A1, n);
+   SetLength(B1, n);  
+   
+  for n := 1 to n do   
    begin
-    Val(P[f],A1[x],pr);
-    inc(x);
-    end;
+    Val(A[n], A1[i], pr);
+    inc(i);
+   end;
+   
+  k := n - m;
+  l := 1;
     
- g:=f-h;
- d:=1;
-   while g <= Length (B1)-1 do 
-     begin 
-      Val(R[d],B1[g],pr);
-      inc(g);
-      inc(d);
+   while k <= Length(B1) - 1 do //создается два массива длины максимального кол-ва разрядов
+      begin
+       Val(B[l], B1[k], pr);
+       inc(k);
+       inc(l);
      end;
    end;
    
-  procedure Calculation (var Rez,A1,B1:array of integer; f,g,s,v:integer); 
+procedure Calculation(var Rez, A1, B1: array of integer; n, m: integer);
+var
+  j, k, v: integer;
    begin
-    SetLength(Rez, f+1); 
-    g := f-1;
-    s := (Length(Rez)-1);
-    while (s >= 0) and (g >= 0) do
-     begin
-      Rez[s]:= Rez[s] + (A1[g]+B1[g])mod 10;
-      v := (A1[g]+B1[g])div 10;
-      if g > 0 then
-       A1[g-1]:= A1[g-1]+v
-      else
-       Rez[s-1]:=Rez[s-1]+v;
-      g:=g-1;
-      s:=s-1;
-   end; 
+    SetLength(Rez, n + 1); 
+    j := n - 1;
+    k := n;
+  
+    while (k >= 0) and (j >= 0) do    
+      begin
+       Rez[k] := Rez[k] + (A1[j] + B1[j]) mod 10; //записывается сумма цифр одного разряда
+       v := (A1[j] + B1[j]) div 10;
+       if j > 0 then
+        A1[j - 1] := A1[j - 1] + v
+       else
+        Rez[k - 1] := Rez[k - 1] + v; 
+       dec(j);
+       dec(k);
+      end; 
   end;
-   
-procedure Output(var Rez: mass; var J:string; n:integer); 
- begin  
-  if Rez[0]=0 then 
-   for n:=1 to Length(Rez)-1 do
-    J:=J+Rez[n]
-  else  
-   for n:=0 to Length(Rez)-1 do
-    J:=J+Rez[n];
-    write( 'Sum of numbers is ', J);
-  end;
-  begin
-   Reading(A,B,n,m);
-   Conversion (A1,B1,f,pr,x,g,d,h, P, R);
-   Calculation (Rez,A1,B1,f,g,s,v);
-   Output(Rez,J,n);
-end.
 
-    
-    
+procedure Output(Rez: mass; var J: string; n: integer);
+ begin
+  if Rez[0] = 0 then 
+   for n := 1 to Length(Rez) - 1 do
+    Rezlt := Rezlt + Rez[n]
+  else  
+   for n := 0 to Length(Rez) - 1 do
+    J := J + Rez[n];
+   write('Sum of numbers is ', Rezlt);
+  end;
+  
+begin
+   Reading(A, B, n, m);
+   Conversion(A1, B1, n, m, A, B);
+   Calculation(Rez, A1, B1, n, m);
+   Output(Rez, Rezlt, n);
+end.
