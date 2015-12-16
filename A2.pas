169222d4f13f6f5937821c055a0311a16e951162
CanNotToCode <vestacaroli@gmail.com>
2015-12-16 19:01:49 +0300

    Update A2.pas

--- A2.pas
+++ A2.pas
@@ -1,30 +1,33 @@
 type 
- Mass = array [1..18] of real;
+ Mass = array  of real;
 var
  A:mass;
- i,k,s:integer;
+ k,n:integer;
+ l:boolean;
  
-procedure Reading (var A:mass);
+procedure Reading (var A:mass; var n:integer);
  var  i:integer;
  begin
- writeln('Enter sequence of 18 elements');
- for i:=1 to 18 do
+ writeln ('Input length of sequence ');
+ readln(n);
+ SetLength(A,n);
+ writeln('Enter sequence of ',n, ' elements');
+ for i:=0 to n-1 do
    read(A[i]);
   end; 
   
-procedure Period (A: Mass);
- var s,k,i,l:integer;
+procedure Period (A: Mass; n:integer; var l:boolean;  var k:integer);
+ var s,i:integer;
  begin  
   k:=1;
- while k<=9 do
+ while k <= (n div 2) do //период не может быть больше половины длины последовательности
   begin 
-  for i:=1 to 18-k do
+  for i:=0 to n-k-1 do
    if A[i]=A[i+k] then
     inc(s);
-   if s = 18-k then
+   if s = n-k then
    begin
-     l:=1;
-     writeln('Period is ', k);
+     l:=true;
      Break;
     end 
     else
@@ -33,12 +36,20 @@ procedure Period (A: Mass);
       s:=0;
     end;
   end;
-  if l = 0 then 
-  write( 'Еhe sequence is not periodic');
  end;
+ 
+procedure Output(l:boolean; k:integer);
+ begin
+  if not l then 
+   write('The sequence is not periodic')
+  else
+   write( 'Period is ',k);
+ end;
+  
 begin
-Reading(A);
-Period(A); 
+ Reading(A,n);
+ Period(A,n,l,k); 
+ Output (l,k);
 end.
     
     
