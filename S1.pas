44f9e3f933cd7204dbae7eb5234415368727f6b7
CanNotToCode <vestacaroli@gmail.com>
2015-12-16 21:14:52 +0300

    Update S1.pas

--- S1.pas
+++ S1.pas
@@ -1,21 +1,46 @@
-var 
- k,i,m:integer;
- a:array[1..1000] of integer;
+//Решето Эратосфена
+type
+  Mass = array of integer;
+
+var
+  k: integer;
+  A: Mass;
+
+procedure Reading(var k: integer);
 begin
-writeln('Enter the border of search of prime numbers');
- read(k);
-write('Prime numbers: ');
-  for i := 2 to (k+1) div 2 do 
+  writeln('Enter the border of search of prime numbers');
+  read(k);
+end;
+
+procedure Search(var A: Mass; k: integer);
+var
+  i, m: integer;
+begin
+  SetLength(a, k + 1);
+  for i := 2 to (k + 1) div 2 do 
   begin
-   m := i*2;
+    m := i * 2;
     while m <= k do
-     begin
-      a[m]:= 1;
-      m := m+i;
+    begin
+      a[m] := 1;
+      m := m + i;
     end;
     m := 0;
   end;
+end;
+
+procedure Output(A: Mass; k: integer);
+var
+  i: integer;
+begin
+  write('Prime numbers: ');
   for i := 1 to k do
-   if a[i] = 0 then write(i,' ');
- readln;
-end. 
+    if a[i] = 0 then write(i, ' ');
+  readln;
+end;
+
+begin
+  Reading(k);
+  Search(A, k);
+  Output(A, k);
+end.
