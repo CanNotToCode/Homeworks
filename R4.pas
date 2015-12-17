936ef2943428a1119f60967a4fbc7262d6667943
CanNotToCode <vestacaroli@gmail.com>
2015-12-17 21:37:48 +0300

    Update R4.pas

--- R4.pas
+++ R4.pas
@@ -11,40 +11,6 @@ var
   n: integer;
   ball: integer;
 
-procedure Reading(var F1: file of abiturient; var n: integer; var ball: integer);
-var
-  i,j: integer;
-  temp: abiturient;
-  m:string;
-begin
-  Assign(F1, 'Points.dat');
-  Rewrite(F1);
-  writeln('Amount of abiturients: ');
-  readln(n);
-  for i := 0 to n - 1 do
-  begin
-    writeln('Input abiturients № ', i + 1); 
-    writeln('FIO:');
-    readln(temp.FIO);
- 
-    writeln('Medal(+ or -): ');
-    readln(m);
-  
-    if m = '+' then
-    temp.medal:=true
-    else temp.medal:=false;
-    
-    writeln('Scores: ');
-    for j:=1 to 3 do
-     readln(temp.Scores[j]);
-    Seek(F1, i);
-    write(F1, temp);
-  end;
-  writeln('Passing score:');
-  read(ball);
-  close(F1);
-end;
-
 procedure Selection(F1: file of abiturient; ball:integer);
 var
   temp: abiturient;
@@ -65,12 +31,14 @@ begin
      end;
     
     if (sum >= ball) or ((temp.medal) and (k>0)) then
-      writeln(temp.FIO);
+      writeln(temp.FIO, ' ', sum);
   end;
 end;
  
 
 begin
-  Reading(F1, n, ball);
+  writeln('Введите проходной балл');
+  read(ball);
+  Assign(F1, 'TestForR4.dat');
   Selection(F1, ball);
 end.
